5b80c8ec336359ec6fa9fd9f91c2a24accaa4777
Nick Huemmer <njhuemmer@gmail.com>
2021-01-12 11:44:59 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const quotes = [
+    {
+        quote: 'Every one of us is, in the cosmic perspective, precious. If a human disagrees with you, let him live. In a hundred billion galaxies, you will not find another.' ,
+        source: 'Carl Sagan',
+    },
+    {
+        quote: 'This is the way',
+        source: 'Din Djarin',
+        citation: 'The Mandalorian',
+        year: 2020
+    },
+    {
+        quote: 'Happiness is not something ready made. It comes from your own actions.',
+        source: 'Dalai Lama XIV',
+    },
+    {
+        quote: 'True happiness is to enjoy the present, without anxious dependence upon the future, not to amuse ourselves with either hopes or fears but to rest satisfied with what we have, which is sufficient, for he that is so wants nothing. The greatest blessings of mankind are within us and within our reach. A wise man is content with his lot, whatever it may be, without wishing for what he has not.',
+        source: 'Seneca',
+    },
+    {
+        quote: 'Friendship ... is born at the moment when one man says to another "What! You too? I thought that no one but myself . . .',
+        source: 'C.S. Lewis',
+        citation: 'The Four Loves',
+        year: 1960
+    },
+    {
+        quote: '\"I wish it need not have happened in my time," said Frodo. \"So do I,\" said Gandalf, \"and so do all who live to see such times. But that is not for them to decide. All we have to decide is what to do with the time that is given us.\" ',
+        source: 'J.R.R. Tolkien',
+        citation: 'The Fellowship of the Ring',
+        year: 1954
+    },
+    {
+        quote: '"Never let the future disturb you. You will meet it, if you have to, with the same weapons of reason which today arm you against the present." ',
+        source: 'Marcus Aurelius',
+        citation: 'Meditations',
+        year: 180 
+    }
+]
